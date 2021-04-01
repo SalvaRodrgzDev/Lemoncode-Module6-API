6bf3db54c4f8ac6f80928515571dfce9598ace44
Salva <salvarodrgzdev@gmail.com>
2021-04-01 18:37:54 +0200

    change hotel to character in router fix rick and morty response type

--- src/pods/character-collection/api/character-collection.api.ts
+++ src/pods/character-collection/api/character-collection.api.ts
@@ -1,11 +1,11 @@
 import Axios from 'axios';
 import { CharacterEntityApi } from './character-collection.api-model';
 
-const url = '/api/character';
+const url = 'https://rickandmortyapi.com/api/character';
 
 export const getCharacterCollection = async (): Promise<CharacterEntityApi[]> => {
-  const { data } = await Axios.get<CharacterEntityApi[]>(url);
-  return data;
+  const { data } = await Axios.get<{ results: CharacterEntityApi[]}>(url);
+  return data.results;
 };
 
 export const deleteCharacter = async (id: string): Promise<boolean> => {
--- src/pods/character/api/character.api.ts
+++ src/pods/character/api/character.api.ts
@@ -1,7 +1,7 @@
 import { Character } from './character.api-model';
 import Axios from 'axios';
 
-const characterListUrl = '/api/character';
+const characterListUrl = 'https://rickandmortyapi.com/api/character';
 
 export const getCharacter = async (id: string): Promise<Character> => {
   const { data } = await Axios.get<Character>(`${characterListUrl}/${id}`);
