9fd9777c622f347feebec3cbd5ff5e7b5d7e093e
Salva <salvarodrgzdev@gmail.com>
2021-04-03 18:18:07 +0200

    add badge to character card

--- src/pods/character-collection/components/character-card.component.tsx
+++ src/pods/character-collection/components/character-card.component.tsx
@@ -1,17 +1,13 @@
 import * as React from 'react';
-import { useHistory } from 'react-router-dom';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import CardActions from '@material-ui/core/CardActions';
-import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar/Avatar';
 import IconButton from '@material-ui/core/IconButton/IconButton';
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
 import { CharacterEntityVm } from '../character-collection.vm';
-import * as classes from './character-card.styles';
+import { StyledBadge } from './character-card.styles';
 
 interface Props {
   character: CharacterEntityVm;
@@ -25,22 +21,22 @@ export const CharacterCard: React.FunctionComponent<Props> = (props) => {
   return (
     <Card>
       <CardHeader
-        avatar={<Avatar aria-label="character">{character.species}</Avatar>}
+        avatar={
+          <StyledBadge
+            overlap="circle"
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'right',
+            }}
+            variant="dot"
+            status={character.status}
+          >
+            <Avatar alt="character avatar" src={character.image} >{character.species}</Avatar>
+          </StyledBadge>
+        }
         title={character.name}
         subheader={character.gender}
       />
-      <CardContent>
-        <div className={classes.content}>
-          <CardMedia
-            image={character.image}
-            title={character.name}
-            style={{ height: 0, paddingTop: '56.25%' }}
-          />
-          <Typography variant="subtitle1" gutterBottom>
-            {character.status}
-          </Typography>
-        </div>
-      </CardContent>
       <CardActions>
         <IconButton onClick={() => onEdit(character.id)}>
           <EditIcon />
--- src/pods/character-collection/components/character-card.styles.ts
+++ src/pods/character-collection/components/character-card.styles.ts
@@ -1,7 +1,44 @@
-import { css } from 'emotion';
+import Badge from '@material-ui/core/Badge';
+import { createStyles, withStyles } from '@material-ui/core/styles';
 
-export const content = css`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-`;
+const applyStatusColor = status => {
+  switch (status) {
+    case 'Alive':
+      return '#44b700'
+    case 'Dead':
+      return 'red'
+    default:
+      return 'orange'
+  }
+}
+
+const styles = createStyles( {
+  badge: {
+    backgroundColor: (props: any) => applyStatusColor(props.status),
+    color: '#44b700',
+    // boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    '&::after': {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      borderRadius: '50%',
+      animation: '$ripple 1.2s infinite ease-in-out',
+      border: '1px solid currentColor',
+      content: '""',
+    },
+  },
+  '@keyframes ripple': {
+    '0%': {
+      transform: 'scale(.8)',
+      opacity: 1,
+    },
+    '100%': {
+      transform: 'scale(2.4)',
+      opacity: 0,
+    },
+  },
+});
+
+export const StyledBadge = withStyles(styles)(Badge);
