49f1da166069a174294f0100bdda3eaa409ef5bd
Salva <salvarodrgzdev@gmail.com>
2021-04-03 18:24:35 +0200

    change apis to json server

--- src/pods/character-collection/api/character-collection.api.ts
+++ src/pods/character-collection/api/character-collection.api.ts
@@ -1,11 +1,11 @@
 import Axios from 'axios';
 import { CharacterEntityApi } from './character-collection.api-model';
 
-const url = 'https://rickandmortyapi.com/api/character';
+const url = '/api/character';
 
 export const getCharacterCollection = async (): Promise<CharacterEntityApi[]> => {
-  const { data } = await Axios.get<{ results: CharacterEntityApi[]}>(url);
-  return data.results;
+  const { data } = await Axios.get<CharacterEntityApi[]>(url);
+  return data;
 };
 
 export const deleteCharacter = async (id: string): Promise<boolean> => {
